5fe19b7a8562dad307323d6af42f91baa6a2ced9
Vengiro <120104662+Vengiro@users.noreply.github.com>
2025-02-13 05:14:36 -0500

    MLP implemented need tests

--- src/main.py
+++ src/main.py
@@ -47,7 +47,7 @@ def tape_fill(func):
 """
 This function is a decorator that is used to fill the operations dictionary with the backward operations.
 """
-def ops_bwd(func):
+def bwd(func):
     assert func.__name__ in operations
     operations[func.__name__ + '_bwd'] = func
     return func
@@ -78,5 +78,148 @@ def backprop():
             arg.gradient = arg.gradient + unbroadcast(grad, arg.shape)
     
     tape.clear()
+
+"""
+Basic operations that are used in the forward and backward passes.
+"""
+@tape_fill
+def add(a, b):
+    return a + b
+
+@bwd
+def add_bwd(a, b, out):
+    return out.gradient, out.gradient
+
+@tape_fill
+def mul(a, b):
+    return a * b
+
+@bwd
+def mul_bwd(a, b, out):
+    return b * out.gradient, a * out.gradient
+
+@tape_fill
+def neg(a):
+    return -a
+
+@bwd
+def neg_bwd(a, out):
+    return -out.gradient
+
+def sub(a, b):
+    return add(a, neg(b))
+
+@tape_fill
+def div(a, b):
+    return a / b
+
+@bwd
+def div_bwd(a, b, out):
+    return 1 / b * out.gradient, -a / b**2 * out.gradient
+
+@tape_fill
+def pow(a, b):
+    return a**b
+
+@bwd
+def pow_bwd(a, b, out):
+    return b * a**(b-1) * out.gradient, a**b * np.log(a) * out.gradient
+
+@tape_fill
+def exp(a):
+    return np.exp(a)
+
+@bwd
+def exp_bwd(a, out):
+    return np.exp(a) * out.gradient
+
+@tape_fill
+def log(a):
+    return np.log(a)
+
+@bwd
+def log_bwd(a, out):
+    return 1 / a * out.gradient
+
+@tape_fill
+def matmul(a, b):
+    return a @ b
+
+@bwd
+def matmul_bwd(a, b, out):
+    return out.gradient @ b.T, a.T @ out.gradient
+
+
+
+"""
+Activation functions.
+"""
+@tape_fill
+def relu(a):
+    return np.maximum(0, a)
+
+@bwd
+def relu_bwd(a, out):
+    return (a > 0) * out.gradient
+
+def sigmoid(a):
+    return div(1, add(1, exp(neg(a))))
+
+def tanh(a):
+    return div(sub(exp(a), exp(neg(a))), add(exp(a), exp(neg(a))))
+
+"""
+Loss functions.
+"""
+
+def L2(x, y):
+    return pow(sub(x, y), 2)
+
+# -y * log(x) - (1 - y) * log(1 - x)
+def BCE(x, y):
+    return neg(add(mul(y, log(x)), mul(sub(1, y), log(sub(1, x)))))
+
+
+
+
+class MLP:
+    def __init__(self, num_layers, layers_size, activation, lossFunc):
+        assert num_layers == len(layers_size) - 1
+        self.W = [array(np.random.randn(layers_size[i], layers_size[i+1])) for i in range(num_layers)]
+        self.b = [array(np.random.randn(1, layers_size[i+1])) for i in range(num_layers)]
+        self.activation = activation
+        self.lossFunc = lossFunc
+
+    def forward(self, x):
+        for i in range(len(self.W)):
+            x = add(matmul(x, self.W[i]), self.b[i])
+            x = self.activation(x)
+        return x
+    
+    def train_epoch(self, train_data, train_labels, lr=0.01):
+        assert len(train_data) == len(train_labels)
+        loss = 0
+
+        # Forward pass accumulate gradients
+        for x, y in zip(train_data, train_labels):
+            x = array(x)
+            y = array(y)
+            out = self.forward(x)
+            loss += self.lossFunc(out, y)
+            out.gradient = self.loss(out, y, True)
+            backprop()
+
+        # Update weights with GD
+        for w, b in zip(self.W, self.b):
+            w -= lr * (w.gradient/len(train_data))
+            b -= lr * (b.gradient/len(train_data))
+
+        # Reset gradients
+        for w, b in zip(self.W, self.b):
+            w.gradient = np.zeros_like(w)
+            b.gradient = np.zeros_like(b)
         
+        return loss
+
+
     
